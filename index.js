432e88897b2ce3e24ee6506d9812380e8cf5de0c
Anish Pokharel <anishpokharel@Anishs-MacBook-Air.local>
2026-04-19 21:35:56 +0545

    implementation

--- index.js
+++ index.js
@@ -25,7 +25,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 app.use(cors({
-    origin: 'http://localhost:4200',
+    // origin: 'http://localhost:4200',
+
+        origin: [
+        'http://localhost:4200',
+        'https://kalikayatayat.netlify.app',
+    ],
+
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
